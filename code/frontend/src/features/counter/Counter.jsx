c08d7c64a00dd476ba40c0c70031bf616be757a7
varadtote <varadtote1111@gmail.com>
2023-12-06 20:19:26 +0530

    redux toolkit boilerplate added

---
+++ code/frontend/src/features/counter/Counter.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { selectCount } from "./counterSlice";
+
+const Counter = () => {
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+  return <div>
+    
+  </div>;
+};
+
+export default Counter;
