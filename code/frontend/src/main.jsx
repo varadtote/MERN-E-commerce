a50cca80dd0e0ad1470931be24cbe569eb483b84
varadtote <varadtote1111@gmail.com>
2023-12-06 19:11:04 +0530

    updated the wrongly inported store

--- code/frontend/src/main.jsx
+++ code/frontend/src/main.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { store } from "./app/store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { store } from './store/store';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
