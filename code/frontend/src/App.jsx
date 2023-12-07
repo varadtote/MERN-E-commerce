f55bae004270e4b5670a551fdab7db11940dd28e
varadtote <varadtote1111@gmail.com>
2023-12-07 19:14:40 +0530

    routing added

--- code/frontend/src/App.jsx
+++ code/frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+
+import { Route, Routes, Link, Outlet } from "react-router-dom";
+import SignupPage from "./pages/SignupPage";
 
 export default function App() {
   return (
     <>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
     </>
   );
 }
