d9a32dfa036b7a337956dbb4a47bd5b6c5b00f76
varadtote <varadtote1111@gmail.com>
2023-12-07 18:40:25 +0530

    STATIC : made home page of entire website

--- code/frontend/src/App.jsx
+++ code/frontend/src/App.jsx
@@ -1,9 +1,10 @@
-
+import React from "react";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <Home />
+    </>
+  );
 }
