cc1eb9d75be4d8ad406cffbfd40feba997ebe2bb
varadtote <varadtote1111@gmail.com>
2023-12-07 18:40:58 +0530

    STATIC : product list page all components added

---
+++ code/frontend/src/features/product-list/ProductListAPI.js
@@ -0,0 +1,11 @@
+import { response } from "express";
+
+export function fetchCount(amount = 1) {
+    return new Promise(async (resolve) => {
+
+        const response = await fetch('http://localhost:8000')
+        const data = await response.json()
+        resolve({ data })
+    }
+    );
+}
